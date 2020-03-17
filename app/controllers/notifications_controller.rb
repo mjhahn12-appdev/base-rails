9d2e4e27e7b4f94d30d93925a6eba7c0fa817472
mjhahn12 <mjhahn@uchicago.edu>
2020-03-17 18:23:55 +0000

    Final
    Deleted notification delete function

--- app/controllers/notifications_controller.rb
+++ app/controllers/notifications_controller.rb
@@ -5,12 +5,4 @@ class NotificationsController < ApplicationController
     render({ :template => "notifications/index.html.erb" })
   end
 
-  def destroy
-    the_id = params.fetch("path_id")
-    @notification = Notification.where({ :id => the_id }).at(0)
-
-    @notification.destroy
-
-    redirect_to("/notifications", { :notice => "Notification deleted successfully."} )
-  end
 end
